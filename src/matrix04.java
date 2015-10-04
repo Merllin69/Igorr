cecdfd82ceb2949d5591bec92dd113644773117f
Игорь Дехнич <igor20121969@gmail.com>
2015-10-04 09:32:00 +0300

    1

---
+++ src/matrix04.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IT_School on 04.10.2015.
+ */
+import java.util.Arrays;
+
+public class matrix04 {
+    public static void main(String[] args) {
+        int[]arr={10,20,30,40};
+        int pos= Arrays.binarySearch(arr,20);
+        System.out.println(pos);
+    }
+}
